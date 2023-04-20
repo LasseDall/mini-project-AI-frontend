49817f5cb73453108bb024f4616710b7573ea048
LasseDall <lass794rrstud.kea.dk>
2023-04-20 13:13:06 +0200

    changeSong added

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ document.getElementById("btn-write-song").onclick = function() {
     writeSong();
 };
 
+document.getElementById("btn-change-song").onclick = function() {
+    changeSong();
+}
+
+async function changeSong() {
+
+    const song = localStorage.getItem("song");
+    const melody = localStorage.getItem("melody");
+    const comment = document.getElementById("changes-input").value
+
+    const changeDTO = {
+        song: song,
+        melody: melody,
+        comment: comment
+    }
+
+    const options = {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(changeDTO)
+    }
+
+    try {
+        const response = await fetch("http://localhost:8080/api/song/change", options).then(res => res.json())
+        document.getElementById("melody-box").innerText = "Melodi: " + response.melody;
+        document.getElementById("song-box").innerText = response.text;
+        localStorage.setItem("song", response.text)
+        localStorage.setItem("melody", response.melody)
+    }catch (err) {
+        console.log(err)
+    }
+}
+
 async function writeSong() {
 
     const melody = document.getElementById("melody-input").value
@@ -28,10 +61,12 @@ async function writeSong() {
     }
 
     try {
-        const response = await fetch("https://mini-project-ai.azurewebsites.net/api/song/write", options).then(res => res.json())
+        const response = await fetch("http://localhost:8080/api/song/write", options).then(res => res.json())
         document.getElementById("melody-box").innerText = "Melodi: " + response.melody;
         document.getElementById("song-box").innerText = response.text;
-        localStorage.setItem("song", response)
+        localStorage.setItem("song", response.text)
+        localStorage.setItem("melody", response.melody)
+        document.getElementById("changes").style.display = "block"
     }catch (err) {
         console.log(err)
     }
